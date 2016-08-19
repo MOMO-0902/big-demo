2b2a8144fb26e785639f16f64c1b6e787b6439eb
Huomenglin <Huomenglin@example.com>
2016-08-19 18:19:35 +0800

    blog return

--- src/components/Blog/Post.js
+++ src/components/Blog/Post.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import IconButton from 'material-ui/IconButton';
+import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
 import marked from 'marked';
 import hljs from 'highlight.js';
+import {browserHistory} from 'react-router';
 class Post extends Component {
   constructor(){
     super();
@@ -23,6 +26,11 @@ class Post extends Component {
     });
   }
 
+  handleBack(){
+    // console.log(hashHistory);
+    browserHistory.goBack();
+  }
+
   render(){
     console.log(this.props.params.title);
     marked.setOptions({
@@ -31,8 +39,36 @@ class Post extends Component {
       }
     });
     let content = marked(this.state.rawContent!='' ? this.state.rawContent : '请稍等......' );
+    let styles={
+      h1:{
+
+        lineHeight:'75px',
+        textAlign:'center',
+        color:'#000',
+        boxShadow: 'rgba(0, 0, 0, 0.15) 0px 1px 6px, rgba(0, 0, 0, 0.15) 0px 1px 4px'
+      },
+      cir:{
+        textAlign:'center'
+      },
+      smallIcon: {
+        width: 32,
+        height: 32,
+      },
+      small: {
+        width: 52,
+        height: 52,
+        padding: 10,
+        position:'absolute',
+        top:'10px',
+        right:'10px',
+      },
+    }
     return(
       <div style={{backgroundColor:'rgba(30,144,255,0.3)'}}>
+      <h1 style={styles.h1}>欢迎阅读</h1>
+        <IconButton iconStyle={styles.smallIcon} style={styles.small} onClick={this.handleBack.bind(this)}>
+          <ArrowBack color='#fff' />
+        </IconButton>
         <div className="post-content">
           <span dangerouslySetInnerHTML={{__html: content}} />
         </div>
